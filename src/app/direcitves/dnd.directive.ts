d7dd43cfc9dc3887ec99d49e9e20b203fa9d7c0a
Pronoy-Anirudhya <anirudhya.sarker@wunderman.com>
2020-06-25 15:13:20 +0600

    refactored codes are being committed from my another workstation

--- src/app/direcitves/dnd.directive.ts
+++ src/app/direcitves/dnd.directive.ts
@@ -11,31 +11,32 @@ import {
 })
 
 export class DndDirective {
-  @HostBinding('class.fileover') fileOver: boolean;
+  @HostBinding('class.fileover') fileDragOver: boolean;
   @Output() fileDropped = new EventEmitter<any>();
 
   // Dragover listener
-  @HostListener('dragover', ['$event']) onDragOver(event: any) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.fileOver = true;
+  @HostListener('dragover', ['$event']) public onDragOver(event: any) {
+    this.handleEvent(event, true);
   }
 
   // Dragleave listener
   @HostListener('dragleave', ['$event']) public onDragLeave(event: any) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.fileOver = false;
+    this.handleEvent(event, false);
   }
 
   // Drop listener
-  @HostListener('drop', ['$event']) public ondrop(event: any) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.fileOver = false;
+  @HostListener('drop', ['$event']) public onDrop(event: any) {
+    this.handleEvent(event, false);
+    
     let files = event.dataTransfer.files;
 
     if (files.length > 0)
       this.fileDropped.emit(files);
+  }
+
+  handleEvent(event: any, fileDragOver: boolean) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileDragOver = fileDragOver;
   }
 }
